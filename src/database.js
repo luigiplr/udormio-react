b569e3250213a79b03b3db9f3c54c765662a1420
Luigi Poole <luigipoole@outlook.com>
2015-12-12 19:06:09 -0800

    add abstraction layer for elastic search calls

--- src/database.js
+++ src/database.js
@@ -1,5 +1,9 @@
 import Promise from 'bluebird';
 import elasticsearch from 'elasticsearch';
+import {
+    v1 as uuid
+}
+from 'node-uuid';
 import config from './config';
 
 const dbClient = new elasticsearch.Client({
@@ -7,16 +11,94 @@ const dbClient = new elasticsearch.Client({
     log: 'trace'
 });
 
-
-
 class Database {
     constructor() {
 
         this.initialized = false;
 
         dbClient.ping(err => {
-            this.initialized = err ? false : true;
+            if (!err)
+                this.init()
+                .then(() => {
+                    this.initialized = true;
+                });
         });
+    }
+
+    init() {
+        return new Promise((resolve, reject) => {
+            dbClient.indices.create('users', {
+                "mappings": {
+                    "profile": {
+                        "properties": {
+                            "email": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            },
+                            "first": {
+                                "type": "string"
+                            },
+                            "last": {
+                                "type": "string"
+                            },
+                            "middle": {
+                                "type": "string"
+                            },
+                            "full": {
+                                "type": "string"
+                            },
+                            "password": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            },
+                            "phone": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            },
+                            "photo": {
+                                "properties": {
+                                    "thumb": {
+                                        "type": "string"
+                                    },
+                                    "time": {
+                                        "type": "double"
+                                    }
+                                }
+                            },
+                            "time": {
+                                "type": "double"
+                            }
+                        }
+                    }
+                }
+            }, err => {
+                if (err) reject(err)
+                else resolve();
+            })
+        });
+    }
+
+    create(index = false, type = false, id = uuid(), params = false, cbType = 'promise') {
+
+        if (!(index || type || params))
+            return 'ERR: input';
+
+        switch (cbType) {
+            case 'promise':
+                return new Promise((resolve, reject) => {
+                    dbClient.create({
+                        index: index,
+                        type: type,
+                        id: id,
+                        body: params
+                    }, (error, response) => {
+                        if (error)
+                            return reject(error);
+                        resolve(response);
+                    });
+                });
+                break;
+        }
     }
 
 }
